c78f2ee4b133df9d751a2105c699f5a41388c5db
Furkan Zerey <furkanzerey96@gmail.com>
2023-12-25 00:52:45 +0300

    fixed

--- user-host/src/main/java/com/fzerey/user/host/middleware/ExceptionMiddleware.java
+++ user-host/src/main/java/com/fzerey/user/host/middleware/ExceptionMiddleware.java
@@ -13,30 +13,29 @@ import com.fzerey.user.shared.exceptions.constants.ExceptionMessages;
 
 @ControllerAdvice
 public class ExceptionMiddleware {
-    
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<?> handleNotFoundException(NotFoundException e) {
-        return new ResponseEntity<ExceptionResponse>(new ExceptionResponse(e.getMessage(), e.getCode()),
+    public ResponseEntity<ExceptionResponse> handleNotFoundException(NotFoundException e) {
+        return new ResponseEntity<>(new ExceptionResponse(e.getMessage(), e.getCode()),
                 HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(AlreadyExistsException.class)
-    public ResponseEntity<?> handleAlreadyExistsException(AlreadyExistsException e) {
-        return new ResponseEntity<ExceptionResponse>(new ExceptionResponse(e.getMessage(), e.getCode()),
+    public ResponseEntity<ExceptionResponse> handleAlreadyExistsException(AlreadyExistsException e) {
+        return new ResponseEntity<>(new ExceptionResponse(e.getMessage(), e.getCode()),
                 HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(UnauthorizedAccessException.class)
-    public ResponseEntity<?> handleUnauthorizedAccessException(UnauthorizedAccessException e) {
-        return new ResponseEntity<ExceptionResponse>(new ExceptionResponse(e.getMessage(), e.getCode()),
+    public ResponseEntity<ExceptionResponse> handleUnauthorizedAccessException(UnauthorizedAccessException e) {
+        return new ResponseEntity<>(new ExceptionResponse(e.getMessage(), e.getCode()),
                 HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> handleException(Exception e) {
+    public ResponseEntity<ExceptionResponse> handleException(Exception e) {
 
-        return new ResponseEntity<ExceptionResponse>(
+        return new ResponseEntity<>(
                 new ExceptionResponse(ExceptionMessages.UNEXPECTED_EXCEPTION, ExceptionCodes.UNEXPECTED_EXCEPTION),
                 HttpStatus.INTERNAL_SERVER_ERROR);
     }
