e2722c0f7a88d0edcdef1e63ac006fe2f75b6615
furkan <furkanzerey96@gmail.com>
2024-01-10 22:52:10 +0300

    removed unused imports

--- user-controller/src/main/java/com/fzerey/user/controller/UserController.java
+++ user-controller/src/main/java/com/fzerey/user/controller/UserController.java
@@ -4,7 +4,6 @@ import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
